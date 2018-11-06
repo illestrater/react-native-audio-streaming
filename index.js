2b14dd5e8b68ccb0c48492862b686e3faf69dea0
bxhu <benjamin.x.hu@gmail.com>
2018-11-05 20:26:18 -0500

    Event listener on Listener exported object

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import {
     NativeModules,
-    StyleSheet,
-    Text,
-    View,
-    TouchableOpacity,
+    NativeAppEventEmitter,
     DeviceEventEmitter,
     ActivityIndicator,
-    Platform
+    Platform,
 } from 'react-native';
+
+import EventEmitter from 'eventemitter3';
 
 const { ReactNativeAudioStreaming } = NativeModules;
 
@@ -26,126 +25,18 @@ const BUFFERING_START = 'BUFFERING_START'; // Android only
 // UI
 const iconSize = 60;
 
-class Player extends Component {
+class Listener extends EventEmitter {
     constructor(props) {
         super(props);
-        this._onPress = this._onPress.bind(this);
-        this.state = {
-            status: STOPPED,
-            song: ''
-        };
-    }
 
-    componentDidMount() {
-        this.subscription = DeviceEventEmitter.addListener(
+        DeviceEventEmitter.addListener(
             'AudioBridgeEvent', (evt) => {
-                // We just want meta update for song name
-                if (evt.status === METADATA_UPDATED && evt.key === 'StreamTitle') {
-                    this.setState({song: evt.value});
-                } else if (evt.status != METADATA_UPDATED) {
-                    this.setState(evt);
+                if (evt.status === PLAYING || evt.status === STOPPED || evt.status === ERROR) {
+                    this.emit(evt.status, evt);
                 }
             }
-        );
-
-        ReactNativeAudioStreaming.getStatus((error, status) => {
-            (error) ? console.log(error) : this.setState(status)
-        });
-    }
-
-    _onPress() {
-        switch (this.state.status) {
-            case PLAYING:
-            case STREAMING:
-                ReactNativeAudioStreaming.pause();
-                break;
-            case PAUSED:
-                ReactNativeAudioStreaming.resume();
-                break;
-            case STOPPED:
-            case ERROR:
-                ReactNativeAudioStreaming.play(this.props.url, {showIniOSMediaCenter: true, showInAndroidNotifications: true});
-                break;
-            case BUFFERING:
-                ReactNativeAudioStreaming.stop();
-                break;
-        }
-    }
-
-    render() {
-        let icon = null;
-        switch (this.state.status) {
-            case PLAYING:
-            case STREAMING:
-                icon = <Text style={styles.icon}>॥</Text>;
-                break;
-            case PAUSED:
-            case STOPPED:
-            case ERROR:
-                icon = <Text style={styles.icon}>▸</Text>;
-                break;
-            case BUFFERING:
-            case BUFFERING_START:
-            case START_PREPARING:
-                icon = <ActivityIndicator
-                    animating={true}
-                    style={{height: 80}}
-                    size="large"
-                />;
-                break;
-        }
-
-        return (
-            <TouchableOpacity style={styles.container} onPress={this._onPress}>
-                {icon}
-                <View style={styles.textContainer}>
-                    <Text style={styles.songName}>{this.state.song}</Text>
-                </View>
-            </TouchableOpacity>
         );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        alignItems: 'center',
-        flexDirection: 'row',
-        height: 80,
-        paddingLeft: 10,
-        paddingRight: 10,
-        borderColor: '#000033',
-        borderTopWidth: 1,
-    },
-    icon: {
-        color: '#000',
-        fontSize: 26,
-        borderColor: '#000033',
-        borderWidth: 1,
-        borderRadius: iconSize / 2,
-        width: iconSize,
-        height: Platform.OS == 'ios' ? iconSize : 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-        paddingTop: Platform.OS == 'ios' ? 10 : 0
-    },
-    textContainer: {
-        flexDirection: 'column',
-        margin: 10
-    },
-    textLive: {
-        color: '#000',
-        marginBottom: 5
-    },
-    songName: {
-        fontSize: 20,
-        textAlign: 'center',
-        color: '#000'
-    }
-});
-
-export { Player, ReactNativeAudioStreaming }
+export { Listener, ReactNativeAudioStreaming }
