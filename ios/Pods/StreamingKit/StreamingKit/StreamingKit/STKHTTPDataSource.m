b948b9d13128b31d95fe9d907952efd98cd44158
bxhu <benjamin.x.hu@gmail.com>
2019-04-30 00:35:52 -0400

    Fix SSL issue

--- ios/Pods/StreamingKit/StreamingKit/StreamingKit/STKHTTPDataSource.m
+++ ios/Pods/StreamingKit/StreamingKit/StreamingKit/STKHTTPDataSource.m
@@ -649,7 +649,6 @@
             NSDictionary* sslSettings = [NSDictionary dictionaryWithObjectsAndKeys:
             (NSString*)kCFStreamSocketSecurityLevelNegotiatedSSL, kCFStreamSSLLevel,
             [NSNumber numberWithBool:NO], kCFStreamSSLValidatesCertificateChain,
-            [NSNull null], kCFStreamSSLPeerName,
             nil];
 
             CFReadStreamSetProperty(stream, kCFStreamPropertySSLSettings, (__bridge CFTypeRef)sslSettings);
